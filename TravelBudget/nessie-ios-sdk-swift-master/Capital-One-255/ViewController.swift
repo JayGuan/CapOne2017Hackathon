af7c1fa289202562d5771face51fba2e78802fb5
jayguan <dongchenguan@gmail.com>
2017-06-07 14:38:52 -0400

    nessie api idk

---
+++ TravelBudget/nessie-ios-sdk-swift-master/Capital-One-255/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Capital-One-255
+//
+//  Created by Jay Guan on 6/7/17.
+//  Copyright © 2017 Nessie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
